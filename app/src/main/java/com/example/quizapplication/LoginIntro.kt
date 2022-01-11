56cc7eb7027749148c98158b7742584f87a9ef25
HARSHMORE093 <moreharsh093@gmail.com>
2022-01-11 20:38:02 +0530

    Integrate Firebase,login and signUp

---
+++ app/src/main/java/com/example/quizapplication/LoginIntro.kt
@@ -0,0 +1,35 @@
+package com.example.quizapplication
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.quizapplication.databinding.ActivityLoginIntroBinding
+import com.google.firebase.auth.FirebaseAuth
+
+class LoginIntro : AppCompatActivity() {
+    lateinit var binding: ActivityLoginIntroBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val auth= FirebaseAuth.getInstance()
+        binding=DataBindingUtil.setContentView(this,R.layout.activity_login_intro)
+        binding.btnGetStarted.setOnClickListener {
+            redirect("LOGIN")
+        }
+        if(auth.currentUser != null){
+            Toast.makeText(this,"User is already logged in!",Toast.LENGTH_SHORT).show()
+            redirect("MAIN")
+        }
+
+    }
+    private fun redirect(name:String){
+        val intent = when (name) {
+            "LOGIN" -> Intent(this, LoginActivity::class.java)
+            "MAIN" -> Intent(this, MainActivity::class.java)
+            else -> throw Exception("no path exists")
+        }
+        startActivity(intent)
+        finish()
+    }
+}
